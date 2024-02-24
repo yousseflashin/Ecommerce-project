21efe717d32939f3b2d1638461516c5db5938b71
yousseflashin <youssef.mlashin.yml@gmail.com>
2024-02-24 18:52:08 +0200

    adding the djagno_filter app to setting file

--- storefront/settings.py
+++ storefront/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'playground',
     'debug_toolbar',
-    'django_filters'
+    'django_filters',
     'rest_framework',
     'store',
     'store_custom',
