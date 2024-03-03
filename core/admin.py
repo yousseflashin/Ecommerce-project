81f83f75fe63c72807bbbc51c2c66c85efbda52d
yousseflashin <youssef.mlashin.yml@gmail.com>
2024-03-03 23:23:37 +0200

    adding and customizing the user class

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from tags.models import Tag ,TaggedItem
 from store.models import Product
 from store.admin import AdminProduct
+from .models import User
 # Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+  add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username","email", "password1", "password2","first_name","last_name"),
+            },
+        ),
+    )
+
 class  TagInLine(GenericTabularInline):
   model = TaggedItem
   autocomplete_fields=['tag']
