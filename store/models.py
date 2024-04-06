86b0cd25d12be14246bfcbad030d6949ead5a5c0
yousseflashin <youssef.mlashin.yml@gmail.com>
2024-04-06 23:33:34 +0200

    image_upload_validator

--- store/models.py
+++ store/models.py
@@ -44,7 +44,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
   product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='image')
   
-  image = models.FileField(upload_to='store/images',validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
+  image = models.ImageField(upload_to='store/images',validators=[validate_file_size])
 
 
 
