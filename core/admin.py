469036c75b06418c28e0c60f6cebeb0bd8e4e63b
youssef <youssef.mlashin.yml@gmail.com>
2024-04-14 15:25:08 +0200

    merging from linux

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
@@ -30,36 +29,3 @@ admin.site.unregister(Product)
 @admin.register(Product)
 class CustomProductAdmin(AdminProduct):
   inlines=[TagInLine,ProductImageInline]
-=======
-from django.contrib import admin
-from django.contrib.contenttypes.admin import GenericTabularInline
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from tags.models import Tag ,TaggedItem
-from store.models import Product
-from store.admin import AdminProduct,ProductImageInline
-from .models import User
-# Register your models here.
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
-  add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("username","email", "password1", "password2","first_name","last_name"),
-            },
-        ),
-    )
-
-class  TagInLine(GenericTabularInline):
-  model = TaggedItem
-  autocomplete_fields=['tag']
-  extra=1
-
-
-admin.site.unregister(Product)
-
-@admin.register(Product)
-class CustomProductAdmin(AdminProduct):
-  inlines=[TagInLine,ProductImageInline]
->>>>>>> 2524bd8b11a92f91b01009e2bf6749044b5b7cf9
--- store/validators.py
+++ store/validators.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 from django.core.exceptions import ValidationError
 
@@ -6,13 +5,4 @@ def validate_file_size(file):
   max_size_gb = 1
 
   if file.size > max_size_gb*1000000000:
-=======
-
-from django.core.exceptions import ValidationError
-
-def validate_file_size(file):
-  max_size_gb = 1
-
-  if file.size > max_size_gb*1000000000:
->>>>>>> 2524bd8b11a92f91b01009e2bf6749044b5b7cf9
       raise ValidationError(f'FILES CANNOT BE LARGER THAN{max_size_gb}gb!')
