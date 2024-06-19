3a07f85330c3c33aa4db06edbfb2ac948824d0a8
yousseflashin <youssef.mlashin.yml@gmail.com>
2024-06-19 18:59:55 +0300

    problem fix

--- store/models.py
+++ store/models.py
@@ -28,7 +28,7 @@ class Product(models.Model):
   title = models.CharField(max_length=255)
   slug=models.SlugField()#to find our product easly by browser
   description=models.TextField(null=True,blank=True,)
-  unit_price=models.DecimalField(max_digits=6,decimal_places=2,validators=[MinValueValidator(1)] )
+  unit_price=models.DecimalField(max_digits=6,decimal_places=2,validators=[MinValueValidator(1.0)] )
   inventory = models.IntegerField(validators=[MinValueValidator(1)])
   last_update = models.DateTimeField(auto_now=True)
   collection=models.ForeignKey(Collection,on_delete=models.PROTECT)
